9dc06ccc9001fd10ae73528e9f9bba0043b2c940
Akash <hafejmohammadakash@gmail.com>
2025-05-22 13:12:45 +0600

    user update method all done onli client site

--- src/components/Update.jsx
+++ src/components/Update.jsx
@@ -12,9 +12,20 @@ const Update = () => {
         const name = form.name.value;
         const email = form.email.value;
 
-        const user = {name, email};
-        console.log(user)
-        
+        const newUser = {name, email};
+        console.log(newUser)
+
+        fetch(`http://localhost:5000/users/${user?._id}`, {
+            method: "PUT",
+            headers: {
+                "content-type" : "application/json"
+            },
+            body: JSON.stringify(newUser)
+        })
+        .then(res => res.json())
+        .then(data => {
+            console.log(data)
+        })
 
         
     }
@@ -26,7 +37,14 @@ const Update = () => {
 
             {/* update user form */}
 
-            
+            <form action="" onSubmit={hanleUpdate}>
+                <input type="text" name="name" defaultValue={user.name} id="" />
+                <br />
+                <input type="email" name="email" defaultValue={user.email} id="" />
+                <br />
+                <button type='submit'>Update user</button>
+            </form>
+
         </div>
     );
 };
